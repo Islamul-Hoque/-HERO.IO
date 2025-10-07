f984c3684c5a3d95106bb21df6a7ed4aab9bcc72
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-10-08 01:32:22 +0600

    Introduce All Applications view with full app cards accessible via navbar

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -5,7 +5,7 @@ const Apps = () => {
     return (
         <div className='m-8 '>
             <h2 className='text-[2.5rem] text-center text-[#001931] font-bold'>Our All Applications</h2>
-            {/* <p className='text-[1.1rem] text-center text-[#627382]'>Explore All Apps on the Market developed by us. We code for Millions</p>
+            <p className='text-[1.1rem] text-center text-[#627382]'>Explore All Apps on the Market developed by us. We code for Millions</p>
 
             <div className=' flex justify-between py-5 items-center'>
                 <h1 className='text-[1.1rem] font-semibold'> (132) Apps Found </h1>
@@ -18,7 +18,7 @@ const Apps = () => {
 
             <div className='grid md:grid-cols-4 mt-3'>
                 <TrendingApps/>
-            </div> */}
+            </div>
         </div>
     );
 };
