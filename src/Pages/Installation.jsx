60403c5531f328c4cf17045ed1edcaafb9118617
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-10-08 01:34:44 +0600

    Display installed apps list under Installation route with status and stats

--- src/Pages/Installation.jsx
+++ src/Pages/Installation.jsx
@@ -7,7 +7,7 @@ const Installation = () => {
             <h2 className='text-[2.5rem] text-center text-[#001931] font-bold'>Our All Applications</h2>
             <p className='text-[1.1rem] text-center text-[#627382]'>Explore All Apps on the Market developed by us. We code for Millions</p>
 
-            {/* <div className=' flex justify-between py-5 items-center'>
+            <div className=' flex justify-between py-5 items-center'>
                 <h1 className='text-[1.1rem] font-semibold'> 1 Apps Found </h1>
                 <label className='form-control w-[10rem] max-w-xs'>
                     <select className='select select-bordered'>
@@ -21,7 +21,7 @@ const Installation = () => {
             <div>
                 <InstalledApps/>
                 <InstalledApps/>
-            </div> */}
+            </div>
         </div>
     );
 };
